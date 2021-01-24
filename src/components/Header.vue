434930bbb6048baca4820093659302f592342d33
Victor_H <Victor.h7@live.com>
2021-01-24 19:26:51 +0100

    Using bootstrap-vue for UI and added API. Added correct API-response title and added next question functionality

---
+++ src/components/Header.vue
@@ -0,0 +1,24 @@
+<template>
+  
+  <div>
+      <b-nav tabs>
+        <b-nav-item disabled><b>Quiz app</b></b-nav-item>
+        <b-nav-item disabled>Counter: 4/10</b-nav-item>
+  </b-nav>
+  </div>
+
+  
+
+</template>
+
+<script>
+
+export default {
+
+}
+
+</script>
+
+<style scoped>
+
+</style>
